cfd4a2553c3c2424e4723249b5908e3b81f0b274
andy00614 <33732843@qq.com>
2020-04-08 23:27:15 +0800

    change inner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import styles from './index.less';
 export default () => {
   return (
     <div>
-      <h1 className={styles.title}>travis-test</h1>
+      <h1 className={styles.title}>andy-test-travis</h1>
     </div>
   );
 };
